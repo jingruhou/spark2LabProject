75fa372885800e7edba64c48c94fe093752a79c7
houjingru <1627383642@qq.com>
2017-09-15 11:17:28 +0800

    添加SparkSQL相关Maven依赖;为项目添加spark2.2.0源码测试数据

---
+++ src/main/scala/com/xunfang/spark2/SparkSQL/SqlSample.scala
@@ -0,0 +1,28 @@
+package com.xunfang.spark2.SparkSQL
+
+import org.apache.spark.sql.SparkSession
+
+
+object SqlSample {
+  def main(args: Array[String]): Unit = {
+    /**
+      * 初始化Spark SQL环境配置
+      */
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL basic example")
+      .master("local[4]")
+      .config("spark.some.config.option","some-value")
+      .getOrCreate()
+
+    /**
+      * For implicit conversions like converting RDDs to DataFrames
+      *
+      * 用于隐式转换，如将RDD转换为DataFrames
+      */
+    import spark.implicits._
+
+    val df = spark.read.json("Resource/examples/people.json")
+    df.show()
+  }
+}
